786e799ec0720d01696b850c974ae099c0cd2d82
Thanh Hoang <thhoang@pdx.edu>
2017-01-28 10:27:13 -0800

    Test git

---
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/BoardGenerate.java
@@ -0,0 +1,12 @@
+package com.pdx.kstn.kstn_boggle;
+
+/**
+ * Created by thanhhoang on 1/28/17.
+ */
+
+public class BoardGenerate {
+    String varDist = "eeeeeeeeeeeeeeeeeeetttttttttttttaaaaaaaaaaaarrrrrrrrrrrriiiiiiiiiiinnnnnnnnnnnooooooooooosssssssssddddddccccchhhhhlllllffffmmmmppppuuuugggyyywwbjkvxzq";
+    String [][] board = new String[][];
+
+
+}
