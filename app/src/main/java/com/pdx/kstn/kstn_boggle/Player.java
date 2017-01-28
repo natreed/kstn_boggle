c457ddb83dae9ad382445fe71353e02f9083f26e
Thanh Hoang <thhoang@pdx.edu>
2017-01-28 12:10:41 -0800

    add: new files

---
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/Player.java
@@ -0,0 +1,8 @@
+package com.pdx.kstn.kstn_boggle;
+
+/**
+ * Created by thanhhoang on 1/28/17.
+ */
+
+public class Player {
+}
---
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/Timer.java
@@ -0,0 +1,12 @@
+package com.pdx.kstn.kstn_boggle;
+
+/**
+ * Created by thanhhoang on 1/28/17.
+ */
+
+public class Timer {
+
+    public boolean updateTime(int scores) {
+        return false;
+    }
+}
