315d49849bb59dbbb4452f5a34d417be321e2d26
kaleb2 <kaleb2@pdx.edu>
2017-02-04 09:13:38 -0800

    Merge branch 'master' of /Users/kalebstriplin/Documents/Andriod Studio/kstn_boggle with conflicts.

--- app/src/main/java/com/pdx/kstn/kstn_boggle/SinglePlayerActivity.java
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/SinglePlayerActivity.java
@@ -1,8 +1,12 @@
 package com.pdx.kstn.kstn_boggle;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * Created by Sharmistha on 1/27/2017.
@@ -23,15 +27,80 @@ public class SinglePlayerActivity extends MainActivity {
             public void onClick(View v) {
                 // Perform action on click
                 // BoggleBoard();
-                board = new char[4][4];
-                String letterdist ="eeeeeeeeeeeeeeeeeeetttttttttttttaaaaaaaaaaaarrrrrrrrrrrriiiiiiiiiiinnnnnnnnnnnooooooooooosssssssssddddddccccchhhhhlllllffffmmmmppppuuuugggyyywwbjkvxzq";
-                for (int row = 0; row < board.length; row++) {
-                    for (int column = 0; column < board.length; column++) {
 
-                        //board[row][column] = (char)('A' + (int)(Math.random()*26));
-                        board[row][column] = letterdist.charAt((int)(Math.random()*(letterdist.length()-1)));
+
+//                board = new char[4][4];
+//                String letterdist ="eeeeeeeeeeeeeeeeeeetttttttttttttaaaaaaaaaaaarrrrrrrrrrrriiiiiiiiiiinnnnnnnnnnnooooooooooosssssssssddddddccccchhhhhlllllffffmmmmppppuuuugggyyywwbjkvxzq";
+//                for (int row = 0; row < board.length; row++) {
+//                    for (int column = 0; column < board.length; column++) {
+//
+//                        //board[row][column] = (char)('A' + (int)(Math.random()*26));
+//                        board[row][column] = letterdist.charAt((int)(Math.random()*letterdist.length()));
+//                    }
+//                }
+
+                board = new char[][] {
+                        {'w', 'a', 't', 'd'},
+                        {'m', 'e', 'j', 't'},
+                        {'h', 'n', 't', 'i'},
+                        {'c', 'l', 'l', 'p'}
+                };
+
+                try {
+                    Resources res = getResources();
+                    InputStream ins = res.openRawResource(R.raw.Dictionary);
+                    Dictionary dic = new Dictionary();
+                    dic.createDictionary(ins);
+
+                    System.out.print("Come here!\n");
+
+                    if (dic.isWord("student"))
+                        System.out.printf("student is in dictionary\n");
+
+                    if (dic.isWord("school"))
+                        System.out.print("school is in dictionary\n");
+
+                    String[] words = new String []{
+                            "amen", "ament", "ate", "awe", "cheat", "chew",
+                            "eat",  "eta", "heat", "hem", "hen", "hent",
+                            "het", "hew", "ill", "item", "jam", "jaw",
+                             "jet", "jew", "jill", "jilt", "lip", "lit",
+                            "lite", "litten", "mae", "mat", "mate", "matt",
+                            "maw", "meat", "men", "met", "meta", "mew",
+                            "neat", "nema", "net", "nett", "new" , "pill",
+                            "pit" ,   "tae" , "taj" , "tam", "tame" , "taw",
+                            "tea" , "team", "teat" , "ten", "tench" , "tent",
+                            "tet", "tew", "til" , "till", "tilt" , "tip",
+                            "tit", "wae" , "wame" , "wat" , "wate" , "watt",
+                             "wen" , "wench" , "went" , "wet"
+                    };
+
+//                    for (int i = 0; i < words.length; i++)
+//                        dic.insert(words[i]);
+
+
+                    for (int i = 0; i < words.length; i++) {
+                        if (dic.isWord(words[i]) == false)
+                            System.out.println(words[i] + ": not found");
                     }
+
+                    long start = System.currentTimeMillis();
+                    ArrayList<String> list = BoggleSolver.solver(board, dic);
+                    long end = System.currentTimeMillis() - start;
+
+                    System.out.println("Total words found:  " + list.size());
+                    System.out.println("Total time: " + (int) end + " miliseconds");
+
+                    for (String word: list)
+                        System.out.println(word);
+
+                    System.out.println("A B C D E F ");
+
+                } catch (Exception e) {
+
                 }
+
+
                 // 1st row
                 Button button0 = (Button) findViewById(R.id.button0);
                 button0.setText(String.valueOf(board[0][0]));
