cbaaf13317ef21cfc89cb3aa8827641dcaf082d4
SharmisthaDutta <dutta@pdx.edu>
2017-02-22 20:58:22 -0800

    add: basic client/server screen

---
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/DoublePlayerActivity.java
@@ -0,0 +1,13 @@
+package com.pdx.kstn.kstn_boggle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoublePlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_double_player);
+    }
+}
