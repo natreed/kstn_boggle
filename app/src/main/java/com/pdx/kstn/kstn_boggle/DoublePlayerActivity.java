70ad446fdcd684728f72127d14cd09556d3d2d48
nathanreed <natreed@pdx.edu>
2017-02-24 14:29:06 -0800

    "Moved timer to player class. Started two player activity."

--- app/src/main/java/com/pdx/kstn/kstn_boggle/DoublePlayerActivity.java
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/DoublePlayerActivity.java
@@ -1,8 +1,3 @@
-package com.pdx.kstn.kstn_boggle;
-
-import android.app.Activity;
-
-public class DoublePlayerActivity extends Activity {
 
 /*
  * Copyright (C) 2014 The Android Open Source Project
