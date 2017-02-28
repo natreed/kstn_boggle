973ced0400cd898404eba2a944a894031ac65a4a
kaleb2 <kaleb2@pdx.edu>
2017-02-27 19:03:37 -0800

    Merge branch 'master' of /Users/kalebstriplin/Documents/Andriod Studio/kstn_boggle with conflicts.

--- app/src/main/java/com/pdx/kstn/kstn_boggle/HighScore.java
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/HighScore.java
@@ -1,16 +1,13 @@
 package com.pdx.kstn.kstn_boggle;
 
-import android.widget.ListView;
-import android.content.res.Resources;
+import android.content.Context;
 
 import java.io.BufferedReader;
 import java.io.FileOutputStream;
-import java.io.File;
-import java.io.InputStream;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Scanner;
 
 import static com.pdx.kstn.kstn_boggle.R.raw.dictionary;
 
@@ -26,59 +23,72 @@ public class HighScore {
     }
 
 
-    public void loadScores(InputStream in) {
+    public void loadScores(FileInputStream fis) {
         // load scores from text file
-
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            StringBuilder sb = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
             String line = null;
             while ((line = reader.readLine()) != null) {
                 scores.add(line);
             }
-            in.close();
+            fis.close();
         } catch (Exception e) {
-
+            System.out.println("Load scores fail\n");
         }
     }
 
     //Check list of scores and update if new high score is available
+    //Always call writeBack after calling update scores for persistence
     public boolean updateScore(String name, int score) {
-        /*Integer newScore = score;
-        Iterator<Tuple> tupleIterator = scores.iterator();
-        while(tupleIterator.hasNext()) {
-            Tuple<String, Integer> entry = tupleIterator.next();
-            if(entry.getScore() < newScore) {
-                tupleIterator.remove();
-                scores.add(new Tuple(name, score));
+        Iterator<String> it = scores.iterator();
+        StringBuilder sb = new StringBuilder();
+        int track = 0;
+        while (it.hasNext()) {
+            String temp = it.next();
+            int len = temp.length();
+            /* this for loops is necessary to extract the score section from
+            // the entire string stored in the txt file. The String is of form
+            // "name: 00" name is of arbitrary so the loops looks through the
+            // String for digits.
+             */
+            for (int i = 0; i < len; i++) {
+                char c = temp.charAt(i);
+                if (Character.isDigit(c)) sb.append(c);
+            }
+            //Int form of the score we found in the list
+            Integer currentScore = Integer.decode(sb.toString());
+            /*
+            // if the score is higher than our current score in the list
+            // remove current score and add it at the current index.
+             */
+            if (score >= currentScore) {
+                StringBuilder newSb = new StringBuilder(name);
+                newSb.append(": ");
+                newSb.append(score);
+                //it.remove();
+                scores.add(track, newSb.toString());
+                scores.remove(scores.size()-1);
                 return true;
             }
-            if(entry.getScore() == newScore) {
-                scores.add(new Tuple(name, score));
-                return true;
-            }
-        }*/
+            track++;
+        }
         return false;
     }
 
+
     //write the scores back to the file
     // format is name: score\n
-    private void writeBack() {
-        /*try {
-            File file = new File("res/raw/scores.txt");
-            FileOutputStream fos = new FileOutputStream(file, false);
-            Iterator<Tuple> tupleIterator = scores.iterator();
-            while(tupleIterator.hasNext()) {
-                Tuple<String, Integer> current = tupleIterator.next();
-                String tempString = current.getName();
-                Integer tempInt = current.getScore();
-                tempString = tempString + ": " + tempInt.toString() +"\n";
-                byte[] content = tempString.getBytes();
-                fos.write(content);
+    public void writeBack(FileOutputStream fos) {
+        try {
+            Iterator<String> it = scores.iterator();
+            while(it.hasNext()) {
+                fos.write(it.next().toString().getBytes());
+                fos.write('\n');
             }
+            fos.close();
         } catch (Exception e) {
-
-        }*/
+            System.out.println("Write back failure\n");
+        }
         return;
     }
 }
--- app/src/main/java/com/pdx/kstn/kstn_boggle/ScoresActivity.java
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/ScoresActivity.java
@@ -1,12 +1,18 @@
 package com.pdx.kstn.kstn_boggle;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 
 /**
@@ -23,17 +29,24 @@ public class ScoresActivity extends MainActivity {
         String[] scores = {};
 
         try {
-            InputStream in = getResources().openRawResource(R.raw.scores);
+            //InputStream in = getResources().openRawResource(R.raw.scores);
+            /*
+            // Add this block of code at the end of the game.
+             */
+            FileInputStream fis = openFileInput("scores.txt");
             HighScore highScore = new HighScore();
-            highScore.loadScores(in);
+            highScore.loadScores(fis);
             scores = highScore.getScores().toArray(new String[0]);
-        } catch (Exception e) { System.out.print("HERE.\n");}
-
-
+            highScore.updateScore("STEVE", 12);
+            FileOutputStream fos = openFileOutput("scores.txt", Context.MODE_PRIVATE);
+            highScore.writeBack(fos);
+        } catch (Exception e) { System.out.println("Scores Activity fail\n");}
 
         ArrayAdapter<String> scoreAdapter = new ArrayAdapter<String>(ScoresActivity.this, android.R.layout.simple_list_item_1, scores);
         ListView scoreList = (ListView) findViewById(R.id.Score_List);
         scoreList.setAdapter(scoreAdapter);
 
+
     }
+
 }
