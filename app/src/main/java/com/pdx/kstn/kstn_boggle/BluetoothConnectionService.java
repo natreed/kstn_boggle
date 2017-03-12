4d7620e98289bb7173dd0e9758b0395b253198aa
nathanreed <natreed@pdx.edu>
2017-03-11 21:12:32 -0800

    "Added copyright to connection service"

--- app/src/main/java/com/pdx/kstn/kstn_boggle/BluetoothConnectionService.java
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/BluetoothConnectionService.java
@@ -16,11 +16,32 @@ package com.pdx.kstn.kstn_boggle;
  import java.util.UUID;
 
 /**
+ * Bluetooth connection Service was Adapted from BluetoothChatService for Boggle project
+ * /*
+ * BlueToothChatService.java
+ * https://developer.android.com/samples/BluetoothChat/src/com.example.android.bluetoothchat/BluetoothChatService.html
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+
  * This class does all the work for setting up and managing Bluetooth
  * connections with other devices. It has a thread that listens for
  * incoming connections, a thread for connecting with a device, and a
  * thread for performing data transmissions when connected.
  */
+
+
 public class BluetoothConnectionService {
 
     // Name for the SDP record when creating server socket
