7a65d5e5e5a585d31696e22024d927d0c005e102
nathanreed <natreed@pdx.edu>
2017-03-17 15:53:37 -0700

    "Revert to Tuesday morning"

--- app/src/main/java/com/pdx/kstn/kstn_boggle/ChooseModeMainActivity.java
+++ app/src/main/java/com/pdx/kstn/kstn_boggle/ChooseModeMainActivity.java
@@ -1,81 +1,91 @@
 package com.pdx.kstn.kstn_boggle;
 
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
-/**
- * Created by thanhhoang on 3/11/17.
- */
-
-public class ChooseModeMainActivity extends Activity{
-
-    public Button btt_startgame;
-    public RadioGroup radioGroup_mode, radioGroup_difficulty;
-    public RadioButton rbtt_mode, rbtt_difficulty;
-
+// Just shows two buttons(single Player,multiplayer) , when user clicks on the buttons it lets users choose a difficulty level and mode (basic play, cutthroat)
+// implemented via using dialog boxes
+public class ChooseModeMainActivity extends AppCompatActivity {
+    //I am replacing the previous comment now.
+    public static int selectedDifficultyLevel=0; //global variable to store state
+    public int selectedDifficultyLevelDoublePlayer=0;
+    public int selectedDoublePlayerMode=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.choose_double_playe_mode);
 
-        initLayout();
-        addListenerOnButtons();
-    }
+        setContentView(R.layout.choose_mode);
+        // hiding action bar
+        //getActionBar().hide();
+
+        final Button single_player_mode = (Button) findViewById(R.id.button_single_player_mode);
+        final Button double_player_mode = (Button) findViewById(R.id.button_double_player_mode);
+
+        single_player_mode.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                // Start NewActivity.class
+               //startActivity(myIntent);
+                //Source of the data in the DIalog
+                CharSequence[] difficultylevel = {"Easy","Medium","Hard"};
+
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder_difficulty_level = new AlertDialog.Builder(ChooseModeMainActivity.this);
 
 
-    private void initLayout() {
-        btt_startgame = (Button) findViewById(R.id.button_start_game);
+                // 2. Chain together various setter methods to set the dialog characteristics
+                       builder_difficulty_level.setSingleChoiceItems(difficultylevel, selectedDifficultyLevel, new DialogInterface.OnClickListener() {
 
-        radioGroup_mode = (RadioGroup) findViewById(R.id.group_mode);
-        radioGroup_difficulty = (RadioGroup) findViewById(R.id.group_difficulty);
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                        // TODO Auto-generated method stub
+                                            selectedDifficultyLevel=which;
+                                            }
+                                        })
+                                        .setTitle(R.string.dialog_title)
+                                         // Set the action buttons
+                                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int id) {
+                                                // User clicked OK, so save the mSelectedItems results somewhere
+                                                // or return them to the component that opened the dialog
+                                                Intent myIntent = new Intent(getApplicationContext(),
+                                                PlayerActivity.class);
+//                                                myIntent.putExtra("difficultylevel", String.valueOf(selectedDifficultyLevel));
+						  myIntent.putExtra("LEVEL", Integer.toString(selectedDifficultyLevel));
+                                                startActivity(myIntent);
+                                            }
+                                        })
+                                                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int id) {
 
-    }
+                                                    }
 
-    private void addListenerOnButtons() {
+                        });
 
-        btt_startgame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                 // get selected radio buttons from groupds (mode, difficulty)
-                int selectedMode = radioGroup_mode.getCheckedRadioButtonId();
-                int selectedDifficulty = radioGroup_difficulty.getCheckedRadioButtonId();
 
-                // find the radiobutton by returned id
-                rbtt_mode = (RadioButton) findViewById(selectedMode);
-                rbtt_difficulty = (RadioButton) findViewById(selectedDifficulty);
-                String str_mode, str_difficulty;
-                str_mode = rbtt_mode.getText().toString().toLowerCase();
-                str_difficulty = rbtt_difficulty.getText().toString().toLowerCase();
+                // 3. Get the AlertDialog from create()
+                                AlertDialog dialog = builder_difficulty_level.create();
+                                dialog.show();
 
-                if (str_mode.length() < 1 || str_difficulty.length() < 1)
-                    return;
-
-                Intent intend;
-
-                if (str_mode.equals("singleplayer"))
-                    intend = new Intent(getApplicationContext(), PlayerActivity.class);
-                else
-                    intend = new Intent(getApplicationContext(), MultiPlayerActivity.class);
-
-                intend.putExtra("MODE", str_mode);
-                intend.putExtra("LEVEL", str_difficulty);
-                startActivity(intend);
             }
         });
 
-    }
 
+        double_player_mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplicationContext(), ChooseModeDouble.class);
+                startActivity(myIntent);
+            }
+        });
 
+   }
 
 }
